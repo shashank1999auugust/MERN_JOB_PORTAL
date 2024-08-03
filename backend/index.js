168c15642f6a1cb1be01145783e7fe7d97e27e8f
shashank1999auugust <sabharwalshashank@gmail.com>
2024-08-03 18:12:04 +0530

    Initial commit message

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors"
+const app= express();
+
+//use this to check whether its running or not http://localhost:3000/home
+app.get("/home",(req,res)=>{
+    return res.status(200).json({
+        message:"I am coming from backend",
+        success:true
+    })
+})
+//middleware
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(cookieParser())
+
+const corsOptions={
+    origin:"http//localhost:5173",
+    credentials:true
+}
+app.use(cors(corsOptions))
+
+const PORT=3000
+app.listen(PORT, ()=>{
+    console.log(`Server running at port ${PORT}`);
+})
